6e5fd1de5e35bf4494a3dcdb565fe1e42b539fdf
Nguyễn Văn Tự Cường <cuongnvtse160875@gmail.com>
2023-07-08 19:44:09 +0700

    fdsffd

--- src/types/auth.type.ts
+++ src/types/auth.type.ts
@@ -34,30 +34,6 @@ export type SignUpRequest = {
     lastName: string
 }
 
-export class SignInBodyApi {
-    @ApiProperty({ example: 'starci@gmail.com', description: 'Email' })
-        email: string
-  
-    @ApiProperty({ example: '123456', description: 'Password' })
-        password: string
-}
-
-export class SignUpRequestApi {
-    @ApiProperty({ example: 'starci@gmail.com', description: 'Email' })
-        email: string
-  
-    @ApiProperty({ example: '123456', description: 'Password' })
-        password: string
-  
-    @ApiProperty({ example: '123456', description: 'Confirm Password' })
-        confirm: string
-  
-    @ApiProperty({ example: 'John', description: 'First Name' })
-        firstName: string
-  
-    @ApiProperty({ example: 'Doe', description: 'Last Name' })
-        lastName: string
-}
 export type SignUpErrors = Partial<{
     emailError: string,
     passwordError: string,
